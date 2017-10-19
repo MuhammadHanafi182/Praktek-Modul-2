c169fc2d224aa3264818b79da36a1205414e05b0
MuhammadHanafi182 <32392643+MuhammadHanafi182@users.noreply.github.com>
2017-10-19 21:49:35 +0700

    Perbaikan Membuat Variabel dengan INISIALISASI nilainya

--- Praktek0202.cpp
+++ Praktek0202.cpp
@@ -3,15 +3,9 @@ using namespace std;
 
 int main(){
 	
-		int nilai=o;
+		int nilai=0;
 		int Nilai=1;
 		int NILAI=2;
-		int nilAi=3;
-		
-		double angka(10,5),Angka,ANGKA(3,14);
-		int a(10),b(7),c(12);
-		
-		cout << nilai<<" : "<<Nilai<<" | "<<NILAI<<" "<<niLAi<<end1;
-		cout << angka <<"\n";
-		cout << Angka <<end1<<ANGKA;
+		int niLAi=3;
+	
 }
